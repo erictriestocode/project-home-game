69668aa0a2a60cdabbdb7dd4ed61983376371ad5
erictriestocode <eric.northrop@gmail.com>
2019-01-26 13:25:19 -0600

    Reorganized JS file and corrected errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 
-//JS File
+// *************** START TICKETMASTER QUERY ***************
+// JS File - Nicole Ajax request
 
 var city = "Chicago";
-var dateStart = "2019-04-01"
-var dateEnd = "2019-04-30"
+var dateStart;
+var dateEnd;
 var idNBA = "KZazBEonSMnZfZ7vFJA";
 var idMLB = "KZazBEonSMnZfZ7vF1n";
 var idNHL = "KZazBEonSMnZfZ7vFEE";
@@ -13,47 +14,46 @@ var idNHL = "KZazBEonSMnZfZ7vFEE";
 // var qurlMLB = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Vvr4nxUQd9eJW45jli8KXF14XyVHA74u&startDateTime=" + dateStart + "T00:00:00Z&endDateTime=" + dateEnd + "T23:59:00Z&city=" + city + "&countryCode=US" + "&subGenreId=" + idMLB; 
 // var qurlNHL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Vvr4nxUQd9eJW45jli8KXF14XyVHA74u&startDateTime=" + dateStart + "T00:00:00Z&endDateTime=" + dateEnd + "T23:59:00Z&city=" + city + "&countryCode=US" + "&subGenreId=" + idNHL;
 // var qurlNFL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Vvr4nxUQd9eJW45jli8KXF14XyVHA74u&startDateTime=" + dateStart + "T00:00:00Z&endDateTime=" + dateEnd + "T23:59:00Z&city=" + city + "&countryCode=US" + "&subGenreId=" + idNFL;
-var qurl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Vvr4nxUQd9eJW45jli8KXF14XyVHA74u&startDateTime=" + dateStart + "T00:00:00Z&endDateTime=" + dateEnd + "T23:59:00Z&city=" + city + "&countryCode=US" + "&subGenreId=" + idNBA + "&subGenreId=" + idMLB + "&subGenreId=" + idNHL; 
+var qurl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Vvr4nxUQd9eJW45jli8KXF14XyVHA74u&startDateTime=" + dateStart + "T00:00:00Z&endDateTime=" + dateEnd + "T23:59:00Z&city=" + city + "&countryCode=US" + "&subGenreId=" + idNBA + "&subGenreId=" + idMLB + "&subGenreId=" + idNHL;
 
 $.ajax({
-  type: "GET",
-  url: qurlMLS,
-  async: true,
-  dataType: "json",
-  success: function (json) {
-    console.log(json);
-  },
-  error: function (xhr, status, err) {
-  }
-=======
-//Create global variables
-var startDate;
-var endDate;
-
-//Remember to do input validation
-function gatherDates() {
-$(".startDate").on("input", function(){
-    startDate = $(".startDate").val();
-    console.log(startDate);
-    alert("you have selected a start date");
+    type: "GET",
+    url: qurl,
+    async: true,
+    dataType: "json",
+    success: function (json) {
+        console.log(json);
+    },
+    error: function (xhr, status, err) {
+    }
 });
 
-//Remember to do input validation
-$(".endDate").on("input", function(){
-    endDate =$(".endDate").val();
-    console.log(endDate);
-    alert("you have selected an end date")
-});
-}
+// *************** END TICKETMASTER QUERY ***************
+
+
+// *************** START USER DATA AQUISITION ***************
+// Remember to do input validation - Eric
+    function gatherDates() {
+        $(".startDate").on("input", function () {
+            dateStart = $(".startDate").val();
+            console.log(dateStart);
+            alert("you have selected a start date");
+        });
+
+        $(".endDate").on("input", function () {
+            dateEnd = $(".endDate").val();
+            console.log(dateEnd);
+            alert("you have selected an end date")
+        });
+    }
 
 gatherDates();
 
-
-//Remember to do input validation
-$(".submit-btn").on("click", function(event){
+$(".submit-btn").on("click", function (event) {
     event.preventDefault();
     gatherDates();
     console.log(startDate);
     console.log(endDate);
 
 });
+// *************** END USER DATA AQUISITION ***************
