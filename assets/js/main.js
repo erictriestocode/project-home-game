0f00bd832ca85d79e6a7c3afec02b4524197ad75
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2019-02-01 23:35:37 -0600

    js-military-time-post-1759

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $("#game-table").hide();
     // Initialize Firebase
@@ -55,7 +54,7 @@ $(document).ready(function () {
         event.preventDefault();
         $("#game-table").show();
         gatherData();
-        getTicketmaster(dateStart,dateEnd,userCity,destCity);
+        getTicketmaster(dateStart, dateEnd, userCity, destCity);
         getMapsInfo(userCity, destCity);
 
         //keep origing city as previously selected and clear out city destination box
@@ -63,53 +62,57 @@ $(document).ready(function () {
         $(".endCty").val("");
     });
 
-        // *************** END USER DATA AQUISITION ***************      
+    // *************** END USER DATA AQUISITION ***************      
 
-        // *************** START TICKETMASTER QUERY ***************
+    // *************** START TICKETMASTER QUERY ***************
 
-        function getTicketmaster(startDate,endDate,origin,destination){
-            var idNBA = "KZazBEonSMnZfZ7vFJA";
-            var idMLB = "KZazBEonSMnZfZ7vF1n";
-            var idNHL = "KZazBEonSMnZfZ7vFEE";
-            var idNFL = "KZazBEonSMnZfZ7vFE1";
-    
-            var qurl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Vvr4nxUQd9eJW45jli8KXF14XyVHA74u&startDateTime=" + startDate + "T00:00:00Z&endDateTime=" + endDate + "T23:59:00Z&city=" + destination + "&countryCode=US" + "&subGenreId=" + idNBA + "&subGenreId=" + idMLB + "&subGenreId=" + idNHL + "&subGenreId=" + idNFL;
-    
-            $.ajax({
-                type: "GET",
-                url: qurl,
-                async: true,
-                dataType: "json",
-                success: function (json) {
-                    console.log(json);
-                    console.log(qurl);
-                    //added if statement to run if events were found in dest city
-                    if(json&&json._embedded&&json._embedded.events&&json._embedded.events.length&& json._embedded.events.length > 0){
+    function getTicketmaster(startDate, endDate, origin, destination) {
+        var idNBA = "KZazBEonSMnZfZ7vFJA";
+        var idMLB = "KZazBEonSMnZfZ7vF1n";
+        var idNHL = "KZazBEonSMnZfZ7vFEE";
+        var idNFL = "KZazBEonSMnZfZ7vFE1";
+
+        var qurl = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=Vvr4nxUQd9eJW45jli8KXF14XyVHA74u&startDateTime=" + startDate + "T00:00:00Z&endDateTime=" + endDate + "T23:59:00Z&city=" + destination + "&countryCode=US" + "&subGenreId=" + idNBA + "&subGenreId=" + idMLB + "&subGenreId=" + idNHL + "&subGenreId=" + idNFL;
+
+        $.ajax({
+            type: "GET",
+            url: qurl,
+            async: true,
+            dataType: "json",
+            success: function (json) {
+                console.log(json);
+                console.log(qurl);
+                //added if statement to run if events were found in dest city
+                if (json && json._embedded && json._embedded.events && json._embedded.events.length && json._embedded.events.length > 0) {
                     // json._embedded.events is an unsorted array containing the event objects. The next line will sort them chronologically.
-                        json._embedded.events.sort(function (a,b){
-                            return new Date(b.dates.start.dateTime) - new Date(a.dates.start.dateTime);
-                        });
-                        function toStandardTime(militaryTime) {
-                            militaryTime = militaryTime.split(':');
-                            if (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2){
-                            militaryTime= (militaryTime[0] - 12) + ':' + militaryTime[1] + ' P.M.';
-                            }else{
+                    json._embedded.events.sort(function (a, b) {
+                        return new Date(b.dates.start.dateTime) - new Date(a.dates.start.dateTime);
+                    });
+                    function toStandardTime(militaryTime) {
+                        militaryTime = militaryTime.split(':');
+                        if (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) {
+                            militaryTime = (militaryTime[0] - 12) + ':' + militaryTime[1] + ' P.M.';
+                        } else if
+                            (militaryTime[0].charAt(0) == 2 && militaryTime[0].charAt(1) >= 0) {
+                            militaryTime = (militaryTime[0] - 12) + ':' + militaryTime[1] + ' P.M.';
+                        }
+                        else {
                             militaryTime.pop();
                             militaryTime.join(':') + ' A.M.';
-                            }
-                            return militaryTime;
-                            }
-                            console.log(toStandardTime('16:30:00'));
-                        for (i = 0; i < json._embedded.events.length; i++) {
-                            name = (json._embedded.events[i].name);
-                            date = (json._embedded.events[i].dates.start.localDate);
-                            time = (json._embedded.events[i].dates.start.localTime);
-                            league = (json._embedded.events[i].classifications[0].subGenre.name);
-                            dateTime = (json._embedded.events[i].dates.start.dateTime)
-    
+                        }
+                        return militaryTime;
+                    }
+                    console.log(toStandardTime('16:30:00'));
+                    for (i = 0; i < json._embedded.events.length; i++) {
+                        name = (json._embedded.events[i].name);
+                        date = (json._embedded.events[i].dates.start.localDate);
+                        time = (json._embedded.events[i].dates.start.localTime);
+                        league = (json._embedded.events[i].classifications[0].subGenre.name);
+                        dateTime = (json._embedded.events[i].dates.start.dateTime)
+
                         //updateHTML
                         $("#table-main").prepend("<tr><td>" + name + "</td><td>" + date + "</td><td>" + toStandardTime(time) + "</td><td>" + league + "</td></tr>");
-                        
+
                         //push to database so we can see what users are searching
                         database.ref().push({
                             dateStart_d: startDate,
@@ -123,11 +126,11 @@ $(document).ready(function () {
                 } else {
                     console.log("no events found");
                 }
-                },
-                error: function (xhr, status, err) {
-                }
-            });
-        };
+            },
+            error: function (xhr, status, err) {
+            }
+        });
+    };
     // *************** END TICKETMASTER QUERY ***************     
 
     // *************** START MAPS QUERY ***************
@@ -150,9 +153,9 @@ $(document).ready(function () {
     };
 
     // *************** UPDATE HTML WITH DISTANCE ***************
- 
+
     // *************** END UPDATE HTML WITH DISTANCE ***************
-    
+
     // *************** END MAPS QUERY ***************
 
     // *************** DATABASE SNAPSHOT ***************
